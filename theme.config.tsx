d548102234a0408ecc92eb3689ad675a2d9a86ab
munaciella <franes88@gmail.com>
2024-08-27 18:19:50 +0100

    initial commit and file structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DevUps Notes</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/munaciella/devups-notes',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/munaciella/devups-notes',
   footer: {
-    text: 'Nextra Docs Template',
+    text: `© ${new Date().getFullYear()} DevUps.  All rights reserved.`,
   },
 }
-
 export default config
