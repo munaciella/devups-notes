7bd2b0efd5c3894e54a3f6a9c55706068e87b31a
munaciella <franes88@gmail.com>
2024-08-29 01:17:23 +0100

    add logo and company menu options

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,59 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+import Head from 'next/head';
 
 const config: DocsThemeConfig = {
-  logo: <span>DevUps Notes</span>,
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img
+        src="/logo.png"
+        alt="DevUps Logo"
+        style={{ height: '52px', marginRight: '8px' }}
+      />
+      <span>DevUps</span>
+    </div>
+  ),
   project: {
     link: 'https://github.com/munaciella/devups-notes',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/munaciella/devups-notes',
-  footer: {
-    text: `© ${new Date().getFullYear()} DevUps.  All rights reserved.`,
+  editLink: {
+    text: null,
   },
-}
-export default config
+  //docsRepositoryBase: 'https://github.com/munaciella/devups-notes',
+  footer: {
+    text: `© ${new Date().getFullYear()} DevUps. All rights reserved.`,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s',
+    };
+  },
+  head: (
+    <Head>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="180x180"
+        href="/assets/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/assets/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/assets/favicon-16x16.png"
+      />
+      <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+    </Head>
+  ),
+};
+
+export default config;
